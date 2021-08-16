f89f16577f6de26b87fe60ef1d8d3753e717c3a6
iikim <iikim2511@gmail.com>
2021-08-15 22:43:33 -0400

    js 58

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// js 58. show navbar with scrollbars
+const navbar = document.querySelector("#navbar");
+
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll",()=>{
+
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');;
+    } else {
+        navbar.classList.remove('navbar--dark');;
+        
+    }
+
+
+});
+
